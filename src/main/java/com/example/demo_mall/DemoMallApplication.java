3fc551f83d818b066de9e74c79f5240a79f2e06f
shoney <ddmczp@naver.com>
2024-01-05 00:37:18 +0900

    intellij ide에서 자동 수정

---
+++ src/main/java/com/example/demo_mall/DemoMallApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoMallApplication.class, args);
+    }
+
+}
