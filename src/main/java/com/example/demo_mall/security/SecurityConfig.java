f88655a1a631fa6ed26840317e5c6e1fad508d45
shoney <ddmczp@naver.com>
2024-01-09 19:07:07 +0900

    [security] logout 및 remember-me 구현

--- src/main/java/com/example/demo_mall/security/SecurityConfig.java
+++ src/main/java/com/example/demo_mall/security/SecurityConfig.java
@@ -3,21 +3,29 @@ package com.example.demo_mall.security;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    UserDetailsService userDetailsService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -27,28 +35,52 @@ public class SecurityConfig {
                 .and()
                 .formLogin(form -> form
 //                        .loginPage("/login")
-                        .defaultSuccessUrl("/members")
-                        .failureUrl("/login.html?error=true")
-                        .usernameParameter("userid")
-                        .passwordParameter("passwd")
-                        .loginProcessingUrl("/login")
-//                        .successHandler(new AuthenticationSuccessHandler() {
-//                            @Override
-//                            public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-//                                System.out.println("authentication :" + authentication.getName());
-//                                response.sendRedirect("/");
-//                            }
-//                        })
-//                        .failureHandler(new AuthenticationFailureHandler() {
-//                            @Override
-//                            public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-//                                System.out.println("exception :" + exception.getMessage());
-//                                response.sendRedirect("/login");
-//                            }
-//                        })
-                        .permitAll()
+                                .defaultSuccessUrl("/members")
+                                .failureUrl("/login.html?error=true")
+                                .usernameParameter("userid")
+                                .passwordParameter("passwd")
+                                .loginProcessingUrl("/login")
+                                .successHandler(new AuthenticationSuccessHandler() {
+                                    @Override
+                                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                                        System.out.println("authentication :" + authentication.getName());
+                                        response.sendRedirect("/");
+                                    }
+                                })
+                                .failureHandler(new AuthenticationFailureHandler() {
+                                    @Override
+                                    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+                                        System.out.println("exception :" + exception.getMessage());
+                                        response.sendRedirect("/login");
+                                    }
+                                })
+                                .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login")
+                        .addLogoutHandler(new LogoutHandler() {
+                            @Override
+                            public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+                                HttpSession session = request.getSession();
+                                session.invalidate();
+                            }
+                        })
+                        .logoutSuccessHandler(new LogoutSuccessHandler() {
+                            @Override
+                            public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                                response.sendRedirect("/login");
+                            }
+                        })
+                        .deleteCookies("remember-me")
+                )
+                .rememberMe(rm -> rm
+                        .rememberMeParameter("remember") // 디폴트 : remember-me
+                        .tokenValiditySeconds(3600) // 디폴트 : 14일
+                        .alwaysRemember(true)
+                        .userDetailsService(userDetailsService)
                 )
                 .build();
     }
 
-}
+} 
