c98375f960f817afffa30bbee870d84b87b4e39e
shoney <ddmczp@naver.com>
2024-02-09 23:23:01 +0900

    feat: 등록, 수정, 삭제 컨트롤러 추가

--- src/main/java/com/example/demo_mall/mallapi/controller/TodoController.java
+++ src/main/java/com/example/demo_mall/mallapi/controller/TodoController.java
@@ -6,10 +6,9 @@ import com.example.demo_mall.mallapi.dto.TodoDto;
 import com.example.demo_mall.mallapi.service.TodoService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @Log4j2
@@ -27,5 +26,25 @@ public class TodoController {
     @GetMapping("/list")
     public PageResDto<TodoDto> getList(PageReqDto pageReqDto) {
         return todoService.getList(pageReqDto);
+    }
+
+    @PostMapping("/")
+    public Map<String, Long> register(@RequestBody TodoDto todoDto) {
+        Long registeredTno = todoService.register(todoDto);
+        return Map.of("tno", registeredTno); // todo : [팀싱크] map으로 반환 해달라고 할때 이렇게 쉽게 사용하는게 낫다
+    }
+
+    @PutMapping("/{tno}")
+    public Map<String, String> modify(@PathVariable("tno") Long tno,
+                                      @RequestBody TodoDto todoDto) {
+        todoDto.setTno(tno);
+        todoService.modify(todoDto);
+        return Map.of("result", "success");
+    }
+
+    @DeleteMapping("/{tno}")
+    public Map<String, String> remove(@PathVariable("tno") Long tno) {
+        todoService.remove(tno);
+        return Map.of("result", "success");
     }
 }
