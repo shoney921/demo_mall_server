0dcdde200d5b45939e4f7e0fc7de785d12bee600
shoney <ddmczp@naver.com>
2024-02-12 00:14:08 +0900

    fix: 쿼리파라가 없는 경우 디폴트 값을 사용하려면, 롬복을 사용해서 구현 가능하도록 수정

--- src/main/java/com/example/demo_mall/mallapi/dto/PageReqDto.java
+++ src/main/java/com/example/demo_mall/mallapi/dto/PageReqDto.java
@@ -1,13 +1,13 @@
 package com.example.demo_mall.mallapi.dto;
 
-import lombok.Builder;
-import lombok.Data;
-import lombok.ToString;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
 
 @ToString
 @Data
 @Builder
+@NoArgsConstructor // Todo : [팀싱크] 쿼리 파라미터가 없을 때, 디폴트 값으로 사용하고 싶으면 NoArgs 사용해야함
+@AllArgsConstructor
 public class PageReqDto {
 
     @Builder.Default
