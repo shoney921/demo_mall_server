0058f8bd462f4e1d059d8f00ad8dbf20215b2f31
shoney <ddmczp@naver.com>
2024-02-12 00:13:24 +0900

    chore: CORS 설정으로 로컬 프론트에서도 접근 가능하도록 변경

--- src/main/java/com/example/demo_mall/mallapi/config/CustomServletConfig.java
+++ src/main/java/com/example/demo_mall/mallapi/config/CustomServletConfig.java
@@ -4,6 +4,7 @@ import com.example.demo_mall.mallapi.formatter.LocalDateFormatter;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -14,5 +15,17 @@ public class CustomServletConfig implements WebMvcConfigurer {
     public void addFormatters(FormatterRegistry registry) {
         log.info("addFormatters 동작 확인");
         registry.addFormatter(new LocalDateFormatter());
+    }
+
+    /**
+     * CORS 설절을 WebMvcConfigurer 메서드 커스텀으로 설정 가능
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*")
+                .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .maxAge(300)
+                .allowedHeaders("Authorization", "Cache-Control", "Content-Type");
     }
 }
