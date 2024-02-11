99cf1d7e7eef7ddbd3e8c92390402cdf00c85356
shoney <ddmczp@naver.com>
2024-02-12 00:14:58 +0900

    feat: 파일 업로드 및 조회 가능한 유틸 구현

---
+++ src/main/java/com/example/demo_mall/mallapi/util/CustomFileUtil.java
@@ -0,0 +1,124 @@
+package com.example.demo_mall.mallapi.util;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import net.coobird.thumbnailator.Thumbnails;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Component
+@Log4j2
+@RequiredArgsConstructor
+public class CustomFileUtil {
+
+    @Value("${org.mall.upload.path}")
+    private String uploadPath;
+
+    @PostConstruct
+    public void init() {
+        File tempFolder = new File(uploadPath);
+
+        if (!tempFolder.exists()) {
+            boolean mkdir = tempFolder.mkdir();
+        }
+
+        uploadPath = tempFolder.getAbsolutePath();
+
+        log.info("파일 경로 : " + uploadPath);
+    }
+
+
+    /**
+     * 파일 저장
+     *
+     * @param files
+     * @return
+     * @throws RuntimeException
+     * @throws IOException
+     */
+    public List<String> saveFiles(List<MultipartFile> files) {
+        if (files == null || files.isEmpty())
+            return null;
+        List<String> uploadNames = new ArrayList<>();
+        try {
+            for (MultipartFile file : files) {
+                String savedName = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
+                Path savePath = Paths.get(uploadPath, savedName);
+                Files.copy(file.getInputStream(), savePath); // 실제 저장
+
+                // 썸네일 저장
+                String contentType = file.getContentType();
+                log.info("contentType = " + contentType);
+                if (contentType != null && contentType.startsWith("image")) {
+                    Path thumbnailPath = Paths.get(uploadPath, "s_" + savedName);
+                    Thumbnails.of(savePath.toFile()).size(200, 200).toFile(thumbnailPath.toFile());
+                }
+
+                uploadNames.add(savedName);
+            }
+        } catch (IOException | RuntimeException e) {
+            throw new RuntimeException(e);
+        }
+        return uploadNames;
+    }
+
+    /**
+     * 파일 조회
+     *
+     * @param fileName
+     * @return
+     * @throws IOException
+     */
+    public ResponseEntity<Resource> getFile(String fileName) {
+        Resource resource = new FileSystemResource(uploadPath + File.separator + fileName);
+
+        if (!resource.isReadable()) {
+            resource = new FileSystemResource(uploadPath + File.separator + "default.jpeg");
+        }
+        HttpHeaders headers = new HttpHeaders();
+
+        try {
+            headers.add("Content-Type", Files.probeContentType(resource.getFile().toPath()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return ResponseEntity.ok().headers(headers).body(resource);
+    }
+
+    /**
+     * 파일 삭제
+     * @param fileNames
+     */
+    public void deleteFiles(List<String> fileNames) {
+        if (fileNames == null || fileNames.isEmpty())
+            return;
+
+        fileNames.forEach(fileName -> {
+            Path thumbnailPath = Paths.get(uploadPath, "s_" + fileName);
+            Path filePath = Paths.get(uploadPath, fileName);
+
+            try {
+                Files.deleteIfExists(filePath);
+                Files.deleteIfExists(thumbnailPath);
+            } catch (IOException e) {
+                throw new RuntimeException(e.getMessage());
+            }
+        });
+    }
+}
