12e440b8bfcc0f19d9eedac00f896f9b0097342c
shoney <ddmczp@naver.com>
2024-03-07 19:04:48 +0900

    feat: 카카오 액세스 토큰을 가지고 맴버 조회하는 컨트롤러 추가

---
+++ src/main/java/com/example/demo_mall/mallapi/controller/SocialController.java
@@ -0,0 +1,22 @@
+package com.example.demo_mall.mallapi.controller;
+
+import com.example.demo_mall.mallapi.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Log4j2
+@RequiredArgsConstructor
+public class SocialController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/api/member/kakao")
+    public String[] getMemberFromKakao(String accessToken) {
+        log.info("accessToken : " + accessToken);
+        memberService.getKakaoMember(accessToken);
+        return new String[]{"sucess", "member", "kakao"};
+    }
+}
