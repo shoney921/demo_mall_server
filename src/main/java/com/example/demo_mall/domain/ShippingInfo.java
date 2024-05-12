c47247a4392acb56635a4ce597a677ad82c7ebd7
shoney <ddmczp@naver.com>
2024-05-12 21:47:18 +0900

    feat : 주문 관련 엔터티 추가

---
+++ src/main/java/com/example/demo_mall/domain/ShippingInfo.java
@@ -0,0 +1,24 @@
+package com.example.demo_mall.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+
+@Embeddable
+public class ShippingInfo {
+
+    @Column(name = "recipient_name", nullable = false)
+    private String recipientName;
+
+    @Column(name = "shipping_address", nullable = false)
+    private String shippingAddress;
+
+    @Column(name = "contact_number", nullable = false)
+    private String contactNumber;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "shipping_status", nullable = false)
+    private ShippingStatus shippingStatus;
+
+}
